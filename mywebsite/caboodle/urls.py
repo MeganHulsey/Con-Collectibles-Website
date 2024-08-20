748d7a4c66656498ea39167d85677e3498e5872d
Megan <megancrowder14@gmail.com>
2024-08-20 13:17:20 -0400

    creating templates

--- mywebsite/caboodle/urls.py
+++ mywebsite/caboodle/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 
 from . import views
 urlpatterns = [
-    path("home/", views.home, name="home"),
-    path("fruit1/", views.fruit1, name="fruit 1"),
-    path("fruit2/", views.fruit2, name="fruit 2"),
-    path("fruit3/", views.fruit3, name="fruit 3"),
+    path("", views.home, name="home"),
+    #path("fruit1/", views.fruit1, name="fruit 1"),
+    #path("fruit2/", views.fruit2, name="fruit 2"),
+    #path("fruit3/", views.fruit3, name="fruit 3"),
 ]
--- mywebsite/caboodle/views.py
+++ mywebsite/caboodle/views.py
@@ -3,14 +3,17 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+def index(response):
+    return render(response, "caboodle/base.html", {})
+
 def home(response):
-    return HttpResponse("<h1>Welcome to Con-Collectibles!</h1>")
+    return render(response, "caboodle/home.html", {})
 
-def fruit1(response):
-    return HttpResponse("Fruit 1!")
+#def fruit1(response):
+    #return HttpResponse("Fruit 1!")
 
-def fruit2(response):
-    return HttpResponse("Fruit 2!")
+#def fruit2(response):
+    #return HttpResponse("Fruit 2!")
 
-def fruit3(response):
-    return HttpResponse("Fruit 3!")
+#def fruit3(response):
+    #return HttpResponse("Fruit 3!")
